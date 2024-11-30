af5a56ed677ae77f3228868d5cc12b03a20176f9
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2024-11-30 08:54:23 +0100

    Fix in file exists check

--- src/main/R/main-scan-net.R
+++ src/main/R/main-scan-net.R
@@ -8,15 +8,16 @@ library(reticulate) # for Python venv --> see InstallingScanNetPythonVenv.txt
 # Adjustable settings #
 #######################
 # Example for Unix
-#rootDir <- "/Users/joeri/git/vkgl-secretome-protein-stability" # root directory that contains README.md, data/, img/, out/, src/, etc.
-#vkglProtVarFileName <- "VKGL_apr2024_protForFolding.tsv" # loading processed VKGL protein variants from: /data/genes/{gene}/{vkglProtVarFile}
-#foldxExec <- "/Applications/FoldX/5/foldx5MacStd/foldx_20241231" # exact path to the FoldX executable
-#p2rankExec <- "/Applications/p2rank_2.5/prank"
+rootDir <- "/Users/joeri/git/vkgl-secretome-protein-stability" # root directory that contains README.md, data/, img/, out/, src/, etc.
+vkglProtVarFileName <- "VKGL_apr2024_protForFolding.tsv" # loading processed VKGL protein variants from: /data/genes/{gene}/{vkglProtVarFile}
+foldxExec <- "/Applications/FoldX/5/foldx5MacStd/foldx_20241231" # exact path to the FoldX executable
+scanNetDir <- "/Users/joeri/git/ScanNet"
+pythonVenv <- "/Users/joeri/git/ScanNet/venv"
+scanNetExe <- "predict_bindingsites.py"
 # Example for Windows
 rootDir <- "D:/github/vkgl-secretome-protein-stability"
 vkglProtVarFileName <- "VKGL_apr2024_protForFolding.tsv"
 foldxExec <- "C:/\"Program Files\"/FoldX/foldx_20241231.exe"
-java <- r"(C:\Program Files\OpenLogic\jdk-22.0.2.9-hotspot\bin\java.exe)"
 scanNetDir <- "D:/github/ScanNet"
 pythonVenv <- "D:\\github\\ScanNet\\venv"
 scanNetExe <- "predict_bindingsites.py"
@@ -40,7 +41,7 @@ succesfulGenes <- unique(results$gene)
 secr <- read.table(file=paste(rootDir, "data", "protein-atlas-secreted-geneIDs-mane-uniprot-withvariants.txt", sep="/"), sep = '\t',header = TRUE)
 intr <- read.table(file=paste(rootDir, "data", "protein-atlas-intracellular-geneIDs-mane-uniprot-random2000-withvariants.txt", sep="/"), sep = '\t',header = TRUE)
 memb <- read.table(file=paste(rootDir, "data", "protein-atlas-membrane-geneIDs-mane-uniprot-random2000-withvariants.txt", sep="/"), sep = '\t',header = TRUE)
-succesfulGenesSub <- Reduce(intersect, list(memb$Gene.name, succesfulGenes))
+succesfulGenesSub <- Reduce(intersect, list(secr$Gene.name, succesfulGenes))
 
 # Iterate over selection of genes
 for(i in seq_along(succesfulGenesSub))
@@ -56,65 +57,66 @@ for(i in seq_along(succesfulGenesSub))
   }
   if(!length(list.files(specificGeneDir, pattern="CombinedScanNetPredictions.csv")) == 0){
     cat("Wild-type CombinedScanNetPredictions.csv file already present, skipping..\n")
-    next
+    
+  }else{
+    
+    tmpDir <- paste(specificGeneDir, "tmp", sep="/")
+    mkdirs(tmpDir)
+    setwd(tmpDir)
+    file.copy(from = paste(specificGeneDir, pdbFile, sep="/"), to = tmpDir)
+    
+    setwd(scanNetDir)
+    
+    scanNetExe_WT_int <- paste0(geneName,"_WT_int_", scanNetExe)
+    scanNetExe_WT_epi <- paste0(geneName,"_WT_epi_", scanNetExe)
+    scanNetExe_WT_idp <- paste0(geneName,"_WT_idp_", scanNetExe)
+    
+    file.copy(from = scanNetExe, to = scanNetExe_WT_int)
+    file.copy(from = scanNetExe, to = scanNetExe_WT_epi)
+    file.copy(from = scanNetExe, to = scanNetExe_WT_idp)
+    
+    oldArgs <- "    args = parser.parse_args()"
+    newArgs_int <- paste0("    args = parser.parse_args([\"",paste(tmpDir, pdbFile, sep="/"),"\", \"--noMSA\", \"--predictions_folder\", \"",tmpDir,"\", \"--mode\", \"interface\"])")
+    newArgs_epi <- paste0("    args = parser.parse_args([\"",paste(tmpDir, pdbFile, sep="/"),"\", \"--noMSA\", \"--predictions_folder\", \"",tmpDir,"\", \"--mode\", \"epitope\"])")
+    newArgs_idp <- paste0("    args = parser.parse_args([\"",paste(tmpDir, pdbFile, sep="/"),"\", \"--noMSA\", \"--predictions_folder\", \"",tmpDir,"\", \"--mode\", \"idp\"])")
+    
+    ok1 <- replace_line_in_file(scanNetExe_WT_int, oldArgs, newArgs_int)
+    ok2 <- replace_line_in_file(scanNetExe_WT_epi, oldArgs, newArgs_epi)
+    ok3 <- replace_line_in_file(scanNetExe_WT_idp, oldArgs, newArgs_idp)
+    
+    if(!(ok1 && ok2 && ok3)){
+      stop("Args replace failed")
+    }
+    
+    py_run_file(scanNetExe_WT_int)
+    py_run_file(scanNetExe_WT_epi)
+    py_run_file(scanNetExe_WT_idp)
+    
+    unlink(scanNetExe_WT_int)
+    unlink(scanNetExe_WT_epi)
+    unlink(scanNetExe_WT_idp)
+    
+    setwd(tmpDir)
+    pdbNoExt <- substr(pdbFile, 1, nchar(pdbFile)-4)
+    intFile <- paste0(tmpDir, "/", pdbNoExt, "_single_ScanNet_interface_noMSA/", "predictions_",pdbNoExt,".csv")
+    epiFile <- paste0(tmpDir, "/", pdbNoExt, "_single_ScanNet_epitope_noMSA/", "predictions_",pdbNoExt,".csv")
+    idpFile <- paste0(tmpDir, "/", pdbNoExt, "_single_ScanNet_idp_noMSA/", "predictions_",pdbNoExt,".csv")
+    combinedFile <- "CombinedScanNetPredictions.csv"
+    combine_last_columns(intFile, epiFile, idpFile, c("int", "epi", "idp"), combinedFile)
+    
+    if(!file.exists(combinedFile)){
+      stop("Combined file for WT failed")
+    }
+    
+    file.copy(from = combinedFile, to = specificGeneDir)
+    
+    if(!file.exists(paste(specificGeneDir, combinedFile, sep="/"))){
+      stop("Combined file for WT not in final location")
+    }
+    
+    setwd(specificGeneDir)
+    unlink(tmpDir, recursive = TRUE)
   }
-  
-  tmpDir <- paste(specificGeneDir, "tmp", sep="/")
-  mkdirs(tmpDir)
-  setwd(tmpDir)
-  file.copy(from = paste(specificGeneDir, pdbFile, sep="/"), to = tmpDir)
-
-  setwd(scanNetDir)
-  
-  scanNetExe_WT_int <- paste0(geneName,"_WT_int_", scanNetExe)
-  scanNetExe_WT_epi <- paste0(geneName,"_WT_epi_", scanNetExe)
-  scanNetExe_WT_idp <- paste0(geneName,"_WT_idp_", scanNetExe)
-
-  file.copy(from = scanNetExe, to = scanNetExe_WT_int)
-  file.copy(from = scanNetExe, to = scanNetExe_WT_epi)
-  file.copy(from = scanNetExe, to = scanNetExe_WT_idp)
-  
-  oldArgs <- "    args = parser.parse_args()"
-  newArgs_int <- paste0("    args = parser.parse_args([\"",paste(tmpDir, pdbFile, sep="/"),"\", \"--noMSA\", \"--predictions_folder\", \"",tmpDir,"\", \"--mode\", \"interface\"])")
-  newArgs_epi <- paste0("    args = parser.parse_args([\"",paste(tmpDir, pdbFile, sep="/"),"\", \"--noMSA\", \"--predictions_folder\", \"",tmpDir,"\", \"--mode\", \"epitope\"])")
-  newArgs_idp <- paste0("    args = parser.parse_args([\"",paste(tmpDir, pdbFile, sep="/"),"\", \"--noMSA\", \"--predictions_folder\", \"",tmpDir,"\", \"--mode\", \"idp\"])")
-  
-  ok1 <- replace_line_in_file(scanNetExe_WT_int, oldArgs, newArgs_int)
-  ok2 <- replace_line_in_file(scanNetExe_WT_epi, oldArgs, newArgs_epi)
-  ok3 <- replace_line_in_file(scanNetExe_WT_idp, oldArgs, newArgs_idp)
-  
-  if(!(ok1 && ok2 && ok3)){
-    stop("Args replace failed")
-  }
-
-  py_run_file(scanNetExe_WT_int)
-  py_run_file(scanNetExe_WT_epi)
-  py_run_file(scanNetExe_WT_idp)
-  
-  unlink(scanNetExe_WT_int)
-  unlink(scanNetExe_WT_epi)
-  unlink(scanNetExe_WT_idp)
-
-  setwd(tmpDir)
-  pdbNoExt <- substr(pdbFile, 1, nchar(pdbFile)-4)
-  intFile <- paste0(tmpDir, "/", pdbNoExt, "_single_ScanNet_interface_noMSA/", "predictions_",pdbNoExt,".csv")
-  epiFile <- paste0(tmpDir, "/", pdbNoExt, "_single_ScanNet_epitope_noMSA/", "predictions_",pdbNoExt,".csv")
-  idpFile <- paste0(tmpDir, "/", pdbNoExt, "_single_ScanNet_idp_noMSA/", "predictions_",pdbNoExt,".csv")
-  combinedFile <- "CombinedScanNetPredictions.csv"
-  combine_last_columns(intFile, epiFile, idpFile, c("int", "epi", "idp"), combinedFile)
-  
-  if(!file.exists(combinedFile)){
-    stop("Combined file for WT failed")
-  }
-  
-  file.copy(from = combinedFile, to = specificGeneDir)
-  
-  if(!file.exists(paste(specificGeneDir, combinedFile, sep="/"))){
-    stop("Combined file for WT not in final location")
-  }
-  
-  setwd(specificGeneDir)
-  unlink(tmpDir, recursive = TRUE)
   
   # Iterate over variants, mutate PDB and predict ligand binding sites
   variants <- read.table(file=paste(specificGeneDir, vkglProtVarFileName, sep="/"), sep = '\t', header = TRUE, colClasses = c("character", "character", "numeric", "character", "character", "character", "character", "numeric", "character"))
