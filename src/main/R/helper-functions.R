9c79831a71737b8ee50b735939e684e5432acd9c
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2024-12-19 20:21:36 +0100

    Move all helper functions to one script

---
+++ src/main/R/helper-functions.R
@@ -0,0 +1,95 @@
+# Replace 3 letter AA codes by 1 letter codes
+aa3to1 <- function(aa3list) {
+  aa1list <- c()
+  for (aa3 in aa3list) {
+    aa1 <- switch(aa3, ALA={"A"}, ARG={"R"}, ASN={"N"}, ASP={"D"}, ASX={"B"}, CYS={"C"}, GLN={"Q"}, GLU={"E"}, GLX={"Z"}, GLY={"G"}, HIS={"H"}, ILE={"I"}, LEU={"L"}, LYS={"K"}, MET={"M"}, PHE={"F"}, PRO={"P"}, SER={"S"}, THR={"T"}, TRP={"W"}, TYR={"Y"}, VAL={"V"})
+    aa1list <- c(aa1list, aa1)
+  }
+  aa1listColl <- paste(aa1list, collapse='')
+  return(aa1listColl)
+}
+
+# Replace 1 letter AA codes by 3 letter codes
+aa1to3 <- function(aa1) {
+  aa3 <- switch(aa1, A={"ALA"}, R={"ARG"}, N={"ASN"}, D={"ASP"}, B={"ASX"}, C={"CYS"}, Q={"GLN"}, E={"GLU"}, Z={"GLX"}, G={"GLY"}, H={"HIS"}, I={"ILE"}, L={"LEU"}, K={"LYS"}, M={"MET"}, F={"PHE"}, P={"PRO"}, S={"SER"}, T={"THR"}, W={"TRP"}, Y={"TYR"}, V={"VAL"})
+  return(aa3)
+}
+
+# In a target file, replace a target string with a replacement string
+replace_line_in_file <- function(file_name, target_string, replacement_string){
+  # Step 1: Read the file into R
+  lines <- readLines(file_name, warn = FALSE)
+  # Step 2: Find the line matching the target string and replace it
+  line_index <- grep(target_string, lines) # Find the line(s) containing the target string
+  if (length(line_index) > 0) {
+    # Replace the matching line(s) with the new string
+    lines[line_index] <- replacement_string
+  } else {
+    warning("No line matches the specified string. No changes were made.")
+    return(FALSE)
+  }
+  # Step 3: Write the modified content back to the file
+  writeLines(lines, file_name)
+  return(TRUE)
+}
+
+# For GLM-Score, combine the results of DNA and RNA binding (features + pKd) into one file
+transpose_and_label <- function(dna_result_file, rna_result_file, dna_pKd, rna_pKd, output_file) {
+  # Read in the files
+  df1 <- read.table(dna_result_file, header = TRUE, sep = "\t")
+  df2 <- read.table(rna_result_file, header = TRUE, sep = "\t")
+  # Add pKD values
+  df1 <- cbind(df1, pKd=dna_pKd)
+  df2 <- cbind(df2, pKd=rna_pKd)
+  # Transpose the data frames
+  transposed_df1 <- t(df1)
+  transposed_df2 <- t(df2)
+  # Convert transposed matrices back to data frames
+  transposed_df1 <- as.data.frame(transposed_df1)
+  transposed_df2 <- as.data.frame(transposed_df2)
+  # Rename the columns for clarity
+  colnames(transposed_df1) <- "DNA"
+  colnames(transposed_df2) <- "RNA"
+  # Combine the two data frames
+  combined_df <- cbind(transposed_df1, transposed_df2)
+  # Write out the new file
+  write.table(combined_df, file = output_file, sep = ",", row.names = TRUE, quote = FALSE)
+}
+
+# For ScanNet output, combine the last columns of 3 result files (interface, epitope, disordered) into a one file
+combine_last_columns <- function(file1, file2, file3, colNames, output_file) {
+  # Read each file
+  data1 <- read.csv(file1)
+  data2 <- read.csv(file2)
+  data3 <- read.csv(file3)
+  # Keep only the last column of each
+  last_col1 <- data1[[ncol(data1)]]
+  last_col2 <- data2[[ncol(data2)]]
+  last_col3 <- data3[[ncol(data3)]]
+  # Combine the last columns into a new data frame
+  combined_data <- data.frame(File1_Last_Column = last_col1,
+                              File2_Last_Column = last_col2,
+                              File3_Last_Column = last_col3)
+  colnames(combined_data) <- colNames
+  # Write the combined data frame to a new file
+  write.csv(combined_data, file = output_file, row.names = FALSE)
+}
+
+# For GeoNet, combine two columns from 3 result files (DNA, RNA, P) into one new file
+combine_geonet_results <- function(file1, file2, file3, colNames, output_file) {
+  # Read each file
+  data1 <- read.csv(file1)
+  data2 <- read.csv(file2)
+  data3 <- read.csv(file3)
+  # Keep probability and binary results
+  probBin1 <- data1[,4:5]
+  probBin2 <- data2[,4:5]
+  probBin3 <- data3[,4:5]
+  # Combine the last columns into a new data frame
+  combined_data <- data.frame(File1_Last_Column = probBin1,
+                              File2_Last_Column = probBin2,
+                              File3_Last_Column = probBin3)
+  colnames(combined_data) <- colNames
+  # Write the combined data frame to a new file
+  write.csv(combined_data, file = output_file, row.names = FALSE)
+}
